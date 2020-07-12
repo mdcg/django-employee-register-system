9a07f982f7754b14d470fa21e3ad0150083f8302
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-07-12 02:52:28 -0300

    Creating view to register employee

--- system/urls.py
+++ system/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from system.views.authentication_views import AuthenticationView, LogoutView
 from system.views.home_views import HomeView
+from system.views.employee_register_views import RegisterEmployeeView
 
 urlpatterns = [
     path('', AuthenticationView.as_view(), name='authentication'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('home/', HomeView.as_view(), name='home'),
+    path('employee/register', RegisterEmployeeView.as_view(), name='employee-register'),
 ]
---
+++ system/views/employee_register_views.py
@@ -0,0 +1,34 @@
+ 
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect, render
+from django.utils.decorators import method_decorator
+from django.views import View
+
+from system.forms.user_forms import UserForm
+
+
+class RegisterEmployeeView(View):
+    @method_decorator(login_required)
+    def get(self, request):
+        user_form = UserForm()
+        context = {
+            "form": user_form,
+        }
+        return render(request, "system/employee_register.html", context)
+
+    @method_decorator(login_required)
+    def post(self, request):
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            messages.success(request, 'Funcionário cadastrado com sucesso!')
+            return redirect("employee-register")
+
+
+        context = {
+            "form": user_form,
+        }
+        return render(request, "system/employee_register.html", context)
